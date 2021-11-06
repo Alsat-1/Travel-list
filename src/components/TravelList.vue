6016dec75fc2da7773655f0ec9106162a37c5a67
Stas <stas.bondarenko@ukr.net>
2021-11-06 08:22:40 +0200

    Get all users from db.json

---
+++ src/components/TravelList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="travels">
+    <h2>Travel list</h2>
+    <div class="travels__cities" v-for="trip in trips" :key="trip.price" v-show="trips.length">
+      <h3 class="travels__cities_title">{{ trip.title }}</h3>
+      <p class="travels__cities_price">Price - {{ trip.price }}</p>
+    </div>
+    <div class="error" v-show="error">{{ error }}</div>
+  </div>
+</template>
+
+<script>
+  import { mapActions, mapState } from 'vuex';
+
+  export default {
+    computed: {
+      ...mapState(['trips', 'error']),
+    },
+    methods: {
+      ...mapActions(['getTripsArray']),
+    },
+    mounted() {
+      this.getTripsArray();
+    },
+  };
+</script>
+
+<style>
+  .error {
+    color: red;
+    font-size: 18px;
+  }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,30 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    trips: [],
+    error: null,
   },
   mutations: {
+    setTrips(state, payload) {
+      state.trips = payload;
+    },
+    setError(state, payload) {
+      state.error = payload;
+    },
   },
+  getters: {},
   actions: {
+    async getTripsArray({ commit }) {
+      try {
+        const trips = await fetch('http://localhost:3000/trips');
+        const response = await trips.json();
+
+        commit('setTrips', response);
+      } catch (err) {
+        commit('setError', err.message);
+      }
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
