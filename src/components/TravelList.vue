24d1b09cdae448adcff11cd956c1369ade9d228b
Stas <stas.bondarenko@ukr.net>
2021-11-07 13:42:07 +0200

    Add a button to switch between EU and All travels

--- src/components/TravelList.vue
+++ src/components/TravelList.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="travels">
-    <h2>Travel list</h2>
-    <div class="travels__cities" v-for="trip in trips" :key="trip.price" v-show="trips.length">
-      <h3 class="travels__cities_title">{{ trip.title }}</h3>
-      <p class="travels__cities_price">Price - {{ trip.price }}</p>
-    </div>
-    <div class="error" v-show="error">{{ error }}</div>
+    <h2 class="travel__title">Travel list</h2>
+    <ul class="travel__list" v-if="travels.length">
+      <li class="travel__item" v-for="travel in travels" :key="travel.id">
+        <h3 class="travel__item_title">{{ travel.title }}</h3>
+        <p class="travel__item_price">Price - {{ travel.price }}</p>
+      </li>
+      <div class="travel__filters">
+        <button class="travel__filters_btn" @click="EUTravels">EU travels</button>
+        <button class="travel__filters_btn" @click="AllTravels">All travels</button>
+      </div>
+    </ul>
+    <div class="error" v-else>{{ error }}</div>
   </div>
 </template>
 
@@ -14,18 +20,62 @@
 
   export default {
     computed: {
-      ...mapState(['trips', 'error']),
+      ...mapState(['travels', 'error']),
     },
     methods: {
-      ...mapActions(['getTripsArray']),
+      ...mapActions(['getTravelsArray']),
+
+      EUTravels() {
+        this.$store.commit('setUrl', 'http://localhost:3000/travels?location=europe');
+        this.$store.dispatch('getTravelsArray');
+      },
+      AllTravels() {
+        this.$store.commit('setUrl', 'http://localhost:3000/travels');
+        this.$store.dispatch('getTravelsArray');
+      },
     },
     mounted() {
-      this.getTripsArray();
+      this.getTravelsArray();
     },
   };
 </script>
 
 <style>
+  .travels {
+    width: 480px;
+    margin: 60px auto;
+  }
+
+  .travel__title {
+    font-size: 26px;
+  }
+
+  .travel__list {
+    padding: 0;
+  }
+
+  .travel__item {
+    border: 1px solid #e4e4e4;
+    box-sizing: border-box;
+    padding: 10px;
+    box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+    list-style-type: none;
+    margin: 20px 0;
+  }
+
+  .travel__filters {
+    display: flex;
+    gap: 10px;
+  }
+
+  .travel__filters_btn {
+    padding: 10px;
+    font-size: 16px;
+    flex-grow: 1;
+    cursor: pointer;
+  }
+
   .error {
     color: red;
     font-size: 18px;
--- src/store/index.js
+++ src/store/index.js
@@ -2,25 +2,29 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
-    trips: [],
+    travels: [],
     error: null,
+    url: 'http://localhost:3000/travels',
   },
   mutations: {
-    setTrips(state, payload) {
-      state.trips = payload;
+    setTravels(state, payload) {
+      state.travels = payload;
     },
     setError(state, payload) {
       state.error = payload;
     },
+    setUrl(state, payload) {
+      state.url = payload;
+    },
   },
   getters: {},
   actions: {
-    async getTripsArray({ commit }) {
+    async getTravelsArray({ commit }) {
       try {
-        const trips = await fetch('http://localhost:3000/trips');
-        const response = await trips.json();
+        const travels = await fetch(this.state.url);
+        const response = await travels.json();
 
-        commit('setTrips', response);
+        commit('setTravels', response);
       } catch (err) {
         commit('setError', err.message);
       }
